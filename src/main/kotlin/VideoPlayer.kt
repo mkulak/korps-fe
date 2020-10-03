75299cb0bb9300d4ff8d934f5a079f7e4fb31c2e
mkulak <kleshney@gmail.com>
2020-10-03 13:31:53 +0200

    video player as functional component

--- src/main/kotlin/VideoPlayer.kt
+++ src/main/kotlin/VideoPlayer.kt
@@ -7,62 +7,60 @@ import styled.css
 import styled.styledButton
 import styled.styledDiv
 
-class VideoPlayer : RComponent<VideoPlayerProps, RState>() {
-    override fun RBuilder.render() {
+val VideoPlayer = functionalComponent<VideoPlayerProps> { props ->
+    styledDiv {
+        css {
+            position = Position.absolute
+            top = 20.px
+            right = 20.px
+        }
+        h3 {
+            +"${props.video.speaker}: ${props.video.title}"
+        }
+        styledButton {
+            css {
+                padding = "10px"
+                marginTop = 10.px
+                marginBottom = 10.px
+                display = Display.block
+                backgroundColor = if (props.video.watched) Color.red else Color.lightGreen
+            }
+            attrs {
+                onClickFunction = {
+                    props.onWatchedButtonPressed(props.video)
+                }
+            }
+            val title = if (props.video.watched) "Mark as unwatched" else "Mark as watched"
+            +title
+        }
         styledDiv {
             css {
-                position = Position.absolute
-                top = 20.px
-                right = 20.px
+                display = Display.flex
+                marginBottom = 10.px
             }
-            h3 {
-                +"${props.video.speaker}: ${props.video.title}"
-            }
-            styledButton {
-                css {
-                    padding = "10px"
-                    marginTop = 10.px
-                    marginBottom = 10.px
-                    display = Display.block
-                    backgroundColor = if (props.video.watched) Color.red else Color.lightGreen
-                }
-                attrs {
-                    onClickFunction = {
-                        props.onWatchedButtonPressed(props.video)
-                    }
-                }
-                val title = if (props.video.watched) "Mark as unwatched" else "Mark as watched"
-                +title
-            }
-            styledDiv {
-                css {
-                    display = Display.flex
-                    marginBottom = 10.px
-                }
-                emailShareButton {
-                    attrs.url = props.video.videoUrl
-                    emailIcon {
-                        attrs.size = 32
-                        attrs.round = true
-                    }
-                }
-                telegramShareButton {
-                    attrs.url = props.video.videoUrl
-                    telegramIcon {
-                        attrs.size = 32
-                        attrs.round = true
-                    }
-                }
-            }
-            reactPlayer {
+            emailShareButton {
                 attrs.url = props.video.videoUrl
+                emailIcon {
+                    attrs.size = 32
+                    attrs.round = true
+                }
             }
+            telegramShareButton {
+                attrs.url = props.video.videoUrl
+                telegramIcon {
+                    attrs.size = 32
+                    attrs.round = true
+                }
+            }
+        }
+        reactPlayer {
+            attrs.url = props.video.videoUrl
         }
     }
 }
 
 fun RBuilder.videoPlayer(handler: VideoPlayerProps.() -> Unit): ReactElement =
-    child(VideoPlayer::class) {
+    child(VideoPlayer) {
         attrs(handler)
     }
 
